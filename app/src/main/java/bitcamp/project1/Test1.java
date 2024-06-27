83278a6fa123fa7a58aef98647c68bffc60db50e
ftfg777 <ftfgz5093@gmail.com>
2024-06-27 15:55:01 +0900

    git Test

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -3,6 +3,7 @@ package bitcamp.project1;
 public class Test1 {
     public static class Print {
         public Print() {
+            System.out.println("git test임둥");
         }
         
         protected void printHello() {
