8cb66e4272dd40220bc20d20c2b796c6ec7d8618
Bulgogi-Pizza <quitendexit@gmail.com>
2024-06-27 15:46:22 +0900

    add Test.java

---
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -0,0 +1,12 @@
+package bitcamp.project1;
+
+public class Test1 {
+    public static class Print {
+        public Print() {
+        }
+        
+        protected void printHello() {
+            System.out.println("Hello");
+        }
+    }
+}
