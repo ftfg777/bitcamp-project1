d4d8f00d42d211cf007171c9cdc549f65faa12a0
ftfg777 <ftfgz5093@gmail.com>
2024-06-27 17:01:05 +0900

    .

--- app/src/main/java/bitcamp/project1/Test1.java
+++ app/src/main/java/bitcamp/project1/Test1.java
@@ -3,16 +3,24 @@ package bitcamp.project1;
 public class Test1 {
     public static class Print {
         public Print() {
-            System.out.println("git test임둥");
+            System.out.println("git test임둥둥둥둥둥둥둥");
         }
 
-        static void m2() {
-            System.out.println("mbti : ENTP");
+        static void bug2(){
+            System.out.println("dragonfly");
         }
-        
+
+        static void bug3(){
+            System.out.println("warm");
+        }
+
         protected void printHello() {
             System.out.println("Hello");
         }
+    }
+
+    static void bug1(){
+        System.out.println("fly");
     }
 
     static void m1(){
